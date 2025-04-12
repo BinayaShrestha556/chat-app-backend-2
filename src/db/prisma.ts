960d5acb9bd75d829f0bd24d3c8b70dfdac39153
BinayaShrestha556 <binayastha37b@gmail.com>
2025-04-12 18:54:33 +0545

    another 1

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../client";
+import { PrismaClient } from "../../dist/client";
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
